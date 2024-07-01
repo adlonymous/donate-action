2b5a61ab99bf3199f7fd70d5e2ebed29100e78bd
adlonymous <adlonymous@gmail.com>
2024-07-01 17:07:25 -0400

    Done ig

---
+++ app/api/actions/transfer-sol/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "FWXHZxDocgchBjADAxSuyPCVhh6fNLT7DUggabAsuz1y" // extra wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
